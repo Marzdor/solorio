02ec762c96a1b18a466c5ef12bef74dbb343cd59
Max Ingersoll <marzdor@gmail.com>
2018-09-05 12:25:08 -0700

    added responsiveness to iframe

--- src/assests/components/Home.js
+++ src/assests/components/Home.js
@@ -4,9 +4,8 @@ const Home = props => {
   return (
     <section>
       <iframe
+        id="video"
         title="Company Video"
-        width="100%"
-        height="250px"
         src="https://www.youtube.com/embed/bgeYi_JDO5k?rel=0&amp;showinfo=0"
         frameBorder="0"
         allow="autoplay; encrypted-media"
